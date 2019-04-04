6d891fb79966434ac81ca273ac278feec4c208f6
wady101 <omegangregovitch@gmail.com>
2019-04-04 09:57:22 -0700

    Daily update :sleeping:

---
+++ scull/kake.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# dmesg                    # dump kernel messages 
+# vim /var/log/kernel.log
+# vim /proc/devices        # devices list
+# ack functionname         # Finding function name in all files ./
+
